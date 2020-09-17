12039da41d69c7717ccc319196030ec670823fc3
AlexeyMazheykin <65315231+AlexeyMazheykin@users.noreply.github.com>
2020-09-17 15:25:33 +0300

    test finish

---
+++ js/main.js
@@ -0,0 +1,29 @@
+(function () {
+    const tabs = document.querySelectorAll('.tab');
+    const forms = document.querySelectorAll('.form');
+    let tabNav;
+
+    const formSelect = function (tabNav) {
+        forms.forEach(item => {
+            if (item.classList.contains(tabNav)) {
+                item.classList.add('form__active');
+            } else {
+                item.classList.remove('form__active');
+            }
+        });
+    };
+
+    const tabsSelect = function () {
+        tabs.forEach(item => {
+            item.classList.remove('tab__active');
+        });
+        tabNav = this.getAttribute('data__form');
+        this.classList.add('tab__active');
+        formSelect(tabNav);
+    };
+
+    tabs.forEach(item => {
+        item.addEventListener('click', tabsSelect)
+    });
+
+}());
